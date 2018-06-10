ab80d1e74930d255152b900ee1156242a80b0ed4
Yonas Ghidei <ghidei@kth.se>
2018-06-10 19:24:51 +0200

    Midway through intergrating ldfi-akka with cli

--- src/main/scala/ldfi/akka/Main.scala
+++ src/main/scala/ldfi/akka/Main.scala
@@ -1,11 +1,99 @@
 package ldfi.akka
 
 import java.io.PrintWriter
+import java.io.File
+import java.nio.file.Files
+import sys.process._
 
 object Main {
 
   def main(args: Array[String]): Unit = {
+    val ctx = processOptions(args.toList, Context())
+
+    val progDir = ctx.programsDir match {
+      case Some(pd) if pd.exists() => pd
+      case Some(pd) => sys.error("Dir: '" + pd.getCanonicalPath + "' does not exist")
+      case None => sys.error("No program directory was given. For usage, use --help")
+    }
+
+    scalafixRewrite(progDir)
+
+    val mainClass = ctx.mainClass match {
+      case Some(mc) if mc.exists() => mc
+      case Some(mc) => sys.error("Main Class: '" + mc.getCanonicalPath + "' does not exist")
+      case None => sys.error("No main class was given. For usage, use --help")
+    }
+
+    val verifyClass = ctx.verifyProg match {
+      case Some(vc) if vc.exists() => vc
+      case Some(vc) => sys.error("Verify Class: '" + vc.getCanonicalPath + "' does not exist")
+      case None => sys.error("No verify class was given. For usage, use --help")
+    }
+
     new PrintWriter("logs.log") {write("");close()}
     Evaluator.evaluate("SimpleDeliv")
+
+
   }
+
+
+  def scalafixRewrite(progDir: File): Unit = {
+    copyProgram(progDir)
+    val rewrite = "./scalafixCli --rules github:KTH/ldfi-akka/v1.0 program/" !
+
+    if(rewrite != 0)
+      sys.error("Errorcode: " + rewrite + "Failed to rewrite directory: " + progDir.getCanonicalPath)
+
+  }
+
+
+  def copyProgram(progDir: File): Unit = {
+    val basePath = "ldfi-akka/program/"
+    val relativePath = progDir.getCanonicalPath.split("src/main/scala").lift(1) match {
+      case Some(path) => path
+      case None => sys.error("Program directory not under src/main/scala but in " + progDir.getCanonicalPath)
+    }
+
+    for (file <- progDir.listFiles()){
+      if(file.isDirectory) copyProgram(file)
+      else Files.copy(file.toPath, new File(basePath + relativePath).toPath)
+    }
+
+  }
+
+
+  def processOptions(args: List[String], ctx: Context): Context = args match {
+    case "--help" :: rest =>
+      displayHelp()
+      sys.exit(0)
+
+    case "-d" :: progDir :: rest =>
+      processOptions(rest, ctx.copy(programsDir = Some(new File(progDir))))
+
+    case "-m" :: mainClass :: rest =>
+      processOptions(rest, ctx.copy(mainClass = Some(new File(mainClass))))
+
+    case "-v" :: verifyProg :: rest =>
+      processOptions(rest, ctx.copy(mainClass = Some(new File(verifyProg))))
+
+    case List.empty => ctx
+
+    case unknown => sys.error("Option '" + unknown + "' was not recognized. For usage, use --help.")
+
+  }
+
+  def displayHelp(): Unit = {
+    println("Usage: ldfi-akka [options] -d <progdir> -m <main class> -v <verify class>")
+    println(" -d <progDir>            target directory for scalafix rewrites")
+    println(" -m <myproject/main>     path to main file ")
+    println(" -v <myproject/verify>   path to file that returns boolean")
+    println("Options include:")
+    println(" --help                  displays this help")
+  }
+
+  case class Context(programsDir: Option[File] = None,
+                     mainClass: Option[File] = None,
+                     verifyProg: Option[File] = None)
 }
+
+
