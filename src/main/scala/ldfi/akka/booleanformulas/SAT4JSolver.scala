8e8c83be57e139c563925f00e5d00dd8f679b8c2
Yonas Ghidei <ghidei@kth.se>
2018-07-02 13:21:01 +0200

    removed trailing white spaces

--- src/main/scala/ldfi/akka/booleanformulas/SAT4JSolver.scala
+++ src/main/scala/ldfi/akka/booleanformulas/SAT4JSolver.scala
@@ -70,14 +70,12 @@ object SAT4JSolver {
     new VecInt(idList.toArray)
   }
 
-
   def removeSuperSets (models: List[Set[Literal]], entire: List[Set[Literal]]): Set[Set[Literal]] = models match {
     case Nil => Set.empty
     case head :: tail =>
       val isSuperSet = entire.filter(_ != head).exists { m => m.subsetOf(head) }
       if(isSuperSet) removeSuperSets(tail, entire)
       else Set(head) ++ removeSuperSets(tail, entire)
-
   }
 
   def printModels(models : Set[Set[Literal]]): Unit =  {
